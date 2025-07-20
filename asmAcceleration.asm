3bee9ec4db5f9ec822e49872aab9d024540efa44
nicole <vokmez.yy@gmail.com>
2025-07-21 00:24:04 +0800

    Update asmAcceleration.asm

--- asmAcceleration.asm
+++ asmAcceleration.asm
@@ -4,6 +4,15 @@ default rel
 global computeAcceleration
 computeAcceleration:
     
+    ; Formula: (Vf - Vi)/T
+    ; extern int computeAcceleration(float initialV, float finalV, float time);
+    ;                                        XMM0           XMM1         XMM2
+    
+    VSUBSD XMM3, XMM1, XMM0         ; XMM3 = XMM1 - XMM0
+    
+    convert_KM/H_to_M/S:
+    
+    
     
     
     RET
