a4a97642e9eab958204fa8441966fd5f357ca0f3
nicole <vokmez.yy@gmail.com>
2025-07-21 00:43:32 +0800

    converting km/h to m/s

--- asmAcceleration.asm
+++ asmAcceleration.asm
@@ -1,6 +1,11 @@
+section .data
+ONEk dd 1000.0
+ONE dd 1.0
+THREEkSIXh dd 3600.0
+
 section .text
 bits 64
-default rel 
+default rel
 global computeAcceleration
 computeAcceleration:
     
@@ -8,11 +13,19 @@ computeAcceleration:
     ; extern int computeAcceleration(float initialV, float finalV, float time);
     ;                                        XMM0           XMM1         XMM2
     
-    VSUBSD XMM3, XMM1, XMM0         ; XMM3 = XMM1 - XMM0
+    VSUBSS XMM3, XMM1, XMM0                                 ; XMM3 = XMM1 - XMM0
     
-    convert_KM/H_to_M/S:
+    convert_KM/H_to_m/s:
+        VMULSS XMM3, XMM3, [ONEk]                           ; XMM3 = (Vf - Vi) * 1000M/H
+        
+        MOVSS XMM1, [ONE]
+        VDIVSS XMM0, XMM1, [THREEkSIXh]                     ; XMM0 = 1m/3600s
+        
+        
+        VMULSS XMM3, XMM3, XMM0                             ; XMM3 = (Vf - Vi) * 1000M/H * (1m/3600s)
+        
+        VDIVSS XMM3, XMM3, XMM2                             ; XMM3 = ((Vf - Vi) * 1000M/H * (1m/3600s)) / time
+        
+        CVTSS2SI EAX, XMM3                                  ; convert single precision floating point to 32 bit integer (4 bytes)
     
-    
-    
-    
-    RET
+        RET
