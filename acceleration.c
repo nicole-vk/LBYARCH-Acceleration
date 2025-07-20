f5c14542ba3bd67df8f2f8370631d5000b58eaaf
nicole <vokmez.yy@gmail.com>
2025-07-21 00:14:39 +0800

    Create acceleration.c

---
+++ acceleration.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef float matrix[3];	// declare 1D matrix
+extern int computeAcceleration(float initialV, float finalV, float time);
+
+
+int main(){
+	int nCars;
+	float initialV, finalV, time;
+	
+	// ask for number of cars
+	scanf("%d", &nCars);
+	
+	// dynamically allocates a 2D array (row x 3).
+	matrix *ptMatrix = malloc(nCars * sizeof(matrix));
+    if (!ptMatrix) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+	
+	
+	// loop per row
+	for(int i = 0; i < nCars; i++){
+		
+		scanf("%f, %f, %f", &initialV, &finalV, &time);
+		
+		ptMatrix[i][0] = initialV;
+		ptMatrix[i][1] = finalV;
+		ptMatrix[i][2] = time;
+	}
+	
+
+	// pass parameter & print output
+	for(int i = 0; i < nCars; i++){
+		int acceleration = computeAcceleration(ptMatrix[i][0], ptMatrix[i][1], ptMatrix[i][2]);		// avoid passing array as it may modify the value
+		
+		printf("%d\n", acceleration);
+	}
+	
+	free(ptMatrix);
+	return 0;
+}
